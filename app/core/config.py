68da432c11b8f6fa2ebae83b933b7ad133eeabcd
Ho011 <zzzsssx0@gmail.com>
2022-04-13 00:26:03 +0300

    Add config.py file.

---
+++ app/core/config.py
@@ -0,0 +1,46 @@
+from functools import lru_cache
+from typing import (
+    Any,
+    Dict,
+    Optional,
+)
+from pydantic import (
+    BaseSettings,
+    PostgresDsn,
+    validator
+)
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(
+        cls, v: Optional[str], values: Dict[str, Any]
+    ) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("DB_USER"),
+            password=values.get("DB_PASSWORD"),
+            host=values.get("DB_HOST"),
+            path=f"/{values.get('DB_NAME') or  ''}",
+        )
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
