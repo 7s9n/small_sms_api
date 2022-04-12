c974aa26c767c847d80e1e09d9aea175fa143d5f
Ho011 <zzzsssx0@gmail.com>
2022-04-13 02:11:18 +0300

    Edit - set nullable to false for [start_date, end_date, is_active] fields.

--- app/models/school_year.py
+++ app/models/school_year.py
@@ -11,6 +11,6 @@ from app.db.base_class import Base
 class SchoolYear(Base):
     id = Column(BigInteger, primary_key=True)
     title = Column(String, index=True, unique=True, nullable=False)
-    start_date = Column(Date)
-    end_date = Column(Date)
-    is_active = Column(Boolean, default=False)
+    start_date = Column(Date, nullable=False)
+    end_date = Column(Date, nullable=False)
+    is_active = Column(Boolean, default=False, nullable=False)
