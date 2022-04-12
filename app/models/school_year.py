0bac35e1c3e438c8500081541e7f1ba4da0bb6c2
Ho011 <zzzsssx0@gmail.com>
2022-04-13 02:06:26 +0300

    Add school year model.

---
+++ app/models/school_year.py
@@ -0,0 +1,16 @@
+from sqlalchemy import (
+    Column,
+    String,
+    Date,
+    Boolean,
+)
+from sqlalchemy.types import BigInteger
+from app.db.base_class import Base
+
+
+class SchoolYear(Base):
+    id = Column(BigInteger, primary_key=True)
+    title = Column(String, index=True, unique=True, nullable=False)
+    start_date = Column(Date)
+    end_date = Column(Date)
+    is_active = Column(Boolean, default=False)
