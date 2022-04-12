e3396944c93705910d844fdc4bb33c6c049f7da3
Ho011 <zzzsssx0@gmail.com>
2022-04-13 01:38:13 +0300

    Edit - add functionality for Generating __tablename__ automatically.

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,3 +1,17 @@
-from sqlalchemy.orm import declarative_base
+from typing import Any
 
-Base = declarative_base()
+import inflect
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+p = inflect.engine()
+
+
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+    # Generate __tablename__ automatically
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return p.plural(cls.__name__.lower())
