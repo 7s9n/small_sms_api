2c14af6445c305962c09ffa1a50950bd6835a554
Ho011 <zzzsssx0@gmail.com>
2022-04-13 02:20:42 +0300

    Edit - import Student model in base.py file.

--- app/db/base.py
+++ app/db/base.py
@@ -5,3 +5,4 @@ from app.db.base_class import Base
 from app.models.grade import Grade  # noqa
 from app.models.subject import Subject  # noqa
 from app.models.school_year import SchoolYear  # noqa
+from app.models.student import Student  # noqa
