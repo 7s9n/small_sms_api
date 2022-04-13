a873e01bf47fcc0fc9cc9141e40f7bd0b70f28f4
Ho011 <zzzsssx0@gmail.com>
2022-04-14 00:46:20 +0300

    Edit - add subjects relationship field.

--- app/models/grade.py
+++ app/models/grade.py
@@ -2,6 +2,7 @@ from sqlalchemy import (
     Column,
     String,
 )
+from sqlalchemy.orm import relationship
 from sqlalchemy.types import SmallInteger
 from app.db.base_class import Base
 
@@ -9,3 +10,5 @@ from app.db.base_class import Base
 class Grade(Base):
     id = Column(SmallInteger, primary_key=True, index=True)
     name = Column(String, unique=True, index=True, nullable=False)
+
+    subjects = relationship('GradeSubject', back_populates='grade')
