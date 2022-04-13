df490dad1da8b948a83830a1a853f491b44370a6
Ho011 <zzzsssx0@gmail.com>
2022-04-14 01:42:46 +0300

    Edit - add guardian_phone_no field in student model.

--- app/models/student.py
+++ app/models/student.py
@@ -16,3 +16,4 @@ class Student(Base):
     last_name = Column(String, nullable=False)
     gender = Column(Boolean, default=True, nullable=False)
     date_of_birth = Column(Date, nullable=False)
+    guardian_phone_no = Column(String, nullable=False)
