4b6e89be183811a43520115cc6828e43bd2ae8d4
Ho011 <zzzsssx0@gmail.com>
2022-04-15 00:09:47 +0300

    Add registration model.

---
+++ app/models/registration.py
@@ -0,0 +1,22 @@
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    String,
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import BigInteger
+from app.db.base_class import Base
+
+
+class Registration(Base):
+    id = Column(BigInteger, primary_key=True)
+    regi_no = Column(String, nullable=False)
+    student_id = Column(ForeignKey('students.id'), nullable=False)
+    grade_id = Column(ForeignKey('grades.id'), nullable=False)
+    school_year_id = Column(ForeignKey('schoolyears.id'), nullable=False)
+    old_registration_id = Column(ForeignKey('registrations.id'), nullable=True)
+
+    student = relationship('Student', back_populates='registrations')
+    grade = relationship("Grade", back_populates='students')
+    school_year = relationship("SchoolYear")
+    old_registration = relationship("Registration", remote_side=[id])
