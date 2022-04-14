f0b959d6f2040dcfe556ca9d7c52896208d89456
Ho011 <zzzsssx0@gmail.com>
2022-04-15 00:14:48 +0300

    Fix importing libraries in base.py file

--- app/db/base.py
+++ app/db/base.py
@@ -1,10 +1,10 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 
-from db.base_class import Base
-from models.grade import Grade  # noqa
-from models.subject import Subject  # noqa
-from models.school_year import SchoolYear  # noqa
-from models.student import Student  # noqa
-from models.grade_subject import GradeSubject  # noqa
-from models.registration import Registration  # noqa
+from .base_class import Base
+from ..models.grade import Grade  # noqa
+from ..models.subject import Subject  # noqa
+from ..models.school_year import SchoolYear  # noqa
+from ..models.student import Student  # noqa
+from ..models.grade_subject import GradeSubject  # noqa
+from ..models.registration import Registration  # noqa
