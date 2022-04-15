0b0d6e8af17e45bd532ed00510dba1d057a4194d
Ho011 <zzzsssx0@gmail.com>
2022-04-16 02:19:15 +0300

    Edit: include subject route.

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
-from app.api.api_v1.routes import grade
+from app.api.api_v1.routes import (
+    grade,
+    subject,
+)
 
 api_router = APIRouter()
 
 api_router.include_router(grade.router)
+api_router.include_router(subject.router)
