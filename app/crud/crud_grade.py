0e11774f22439ff99875806c294166ec1cd0ba1d
Ho011 <zzzsssx0@gmail.com>
2022-04-18 02:28:31 +0300

    Edit: add new method to retrieve assigned or not assigned grade's subjects

--- app/crud/crud_grade.py
+++ app/crud/crud_grade.py
@@ -1,15 +1,30 @@
-from typing import Optional
+from typing import List, Optional
 from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
-from app.db.base import Grade
+from app.db.base import (
+    Grade,
+    Subject,
+    GradeSubject,
+)
 from app.schemas import (
     GradeCreate,
     GradeUpdate,
 )
 
+
 class CRUDGrade(CRUDBase[Grade, GradeCreate, GradeUpdate]):
     def get_by_name(self, db: Session, *, name: str) -> Optional[Grade]:
         return db.query(self.model).filter(Grade.name == name).first()
+
+    def get_grade_assigned_or_not_assigned_subjects(self, db: Session, *, grade_id: int, assigned: bool = True) -> Optional[List[Subject]]:
+        subquery = db.query(GradeSubject.subject_id).filter(
+            GradeSubject.grade_id == grade_id
+        ).subquery()
+        if assigned:
+            subjects = db.query(Subject).filter(Subject.id.in_(subquery)).all()
+        else:
+            subjects = db.query(Subject).filter(Subject.id.notin_(subquery)).all()
+        return subjects
 
 
 grade = CRUDGrade(Grade)
