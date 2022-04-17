88623f434eea928da06ba0ad15ce45610aea7d57
Ho011 <zzzsssx0@gmail.com>
2022-04-18 02:18:12 +0300

    Edit: format grade.py file.

--- app/schemas/grade.py
+++ app/schemas/grade.py
@@ -1,13 +1,18 @@
+from typing import List
 from pydantic import BaseModel
+
 
 class GradeBase(BaseModel):
     name: str
 
+
 class GradeCreate(GradeBase):
     pass
 
+
 class GradeUpdate(GradeBase):
     pass
+
 
 class GradeInDB(GradeBase):
     id: int
