89289269defc5eb421f24dc2854737e887fbf57e
Ho011 <zzzsssx0@gmail.com>
2022-04-20 00:56:37 +0300

    Edit: add delete cascade for grade

--- app/models/grade.py
+++ app/models/grade.py
@@ -11,5 +11,5 @@ class Grade(Base):
     id = Column(SmallInteger, primary_key=True, index=True)
     name = Column(String, unique=True, index=True, nullable=False)
 
-    subjects = relationship('GradeSubject', back_populates='grade')
+    subjects = relationship('GradeSubject', back_populates='grade', cascade="all, delete-orphan")
     students = relationship('Registration', back_populates='grade')
