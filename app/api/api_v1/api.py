ebddf2df62039e09bc39db949dc08cc179203b4b
Ho011 <zzzsssx0@gmail.com>
2022-04-19 03:03:47 +0300

    import and include the router of school years

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from app.api.api_v1.routes import (
     grade,
     subject,
+    school_year,
 )
 
 api_router = APIRouter()
 
 api_router.include_router(grade.router)
 api_router.include_router(subject.router)
+api_router.include_router(school_year.router)
