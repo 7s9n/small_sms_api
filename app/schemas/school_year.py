cdc9e9841a90e883427f7b1826775b88ebb06e44
Ho011 <zzzsssx0@gmail.com>
2022-04-19 03:00:06 +0300

    new: add school_year schemas.

---
+++ app/schemas/school_year.py
@@ -0,0 +1,58 @@
+from datetime import date
+from datetime import datetime
+from pydantic import (
+    BaseModel,
+    validator
+)
+
+
+class Base(BaseModel):
+    title: str
+    start_date: date
+    end_date: date
+    is_active: bool
+
+    @validator("title")
+    def validate_title(cls, value):
+        if not value:
+            raise ValueError('Title must not be empty.')
+        return value
+
+    @validator("start_date", pre=True)
+    def parse_start_date(cls, value):
+        if isinstance(value, date):
+            return value
+        return datetime.strptime(
+            value,
+            "%Y-%m-%d"
+        ).date()
+
+    @validator("end_date", pre=True)
+    def parse_end_date(cls, value):
+        if isinstance(value, date):
+            return value
+        return datetime.strptime(
+            value,
+            "%Y-%m-%d"
+        ).date()
+
+    @validator("end_date")
+    def validate_start_and_end_date(cls, v, values):
+        if values['start_date'] >= v:
+            raise ValueError('End date is bigger than or equal to start date.')
+        return v
+
+
+class SchoolYearCreate(Base):
+    pass
+
+
+class SchoolYearUpdate(Base):
+    pass
+
+
+class SchoolYearInDB(Base):
+    id: int
+
+    class Config:
+        orm_mode = True
