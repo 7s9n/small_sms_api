cb55c2483b4d73bf78c6fb1bd2e54059e0df6c25
Ho011 <zzzsssx0@gmail.com>
2022-04-23 02:24:15 +0300

    Edit: import student and registration routes.

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from app.api.api_v1.routes import (
     grade,
+    registration,
     subject,
     school_year,
+    student,
 )
 
 api_router = APIRouter()
@@ -10,3 +12,5 @@ api_router = APIRouter()
 api_router.include_router(grade.router)
 api_router.include_router(subject.router)
 api_router.include_router(school_year.router)
+api_router.include_router(student.router)
+api_router.include_router(registration.router)
