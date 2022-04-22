28cc1b90cb851f5e0c04b767f66b2255145be5dd
Ho011 <zzzsssx0@gmail.com>
2022-04-23 02:21:06 +0300

    Edit: import student and registration crud in __init__.py file.

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -2,3 +2,5 @@ from .crud_grade import grade
 from .crud_subject import subject
 from .crud_grade_subject import grade_subject
 from .crud_school_year import school_year
+from .crud_student import student
+from .crud_registration import registeration
