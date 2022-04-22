1ead457dae7d36f7bc4de6da7000972033063d3f
Ho011 <zzzsssx0@gmail.com>
2022-04-23 02:22:22 +0300

    Edit: add CommonQueryParams class dependency.

--- app/api/deps.py
+++ app/api/deps.py
@@ -2,9 +2,16 @@ from typing import Generator
 from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
 
-def get_db()-> Generator:
+
+def get_db() -> Generator:
     try:
         db: Session = SessionLocal()
         yield db
     finally:
         db.close()
+
+
+class CommonQueryParams:
+    def __init__(self, skip: int = 0, limit: int = 100) -> None:
+        self.skip = skip
+        self.limit = limit
