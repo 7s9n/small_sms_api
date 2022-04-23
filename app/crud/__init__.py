ce64ea47d1588be61f622a0619c794660c1197ad
Ho011 <zzzsssx0@gmail.com>
2022-04-24 01:13:56 +0300

    Edit: import nationality instance in __init__.py file.

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -4,3 +4,4 @@ from .crud_grade_subject import grade_subject
 from .crud_school_year import school_year
 from .crud_student import student
 from .crud_registration import registeration
+from .crud_nationality import nationality
