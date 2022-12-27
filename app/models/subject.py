ee797872bd9a5e05280736c29406f09f80dd5e3d
“ho011” <zzzsssx0@gmail.com>
2022-12-27 04:03:11 +0000

    Edit: add columns (add_to_total, higher_score, lower_score) in subject table.

--- app/models/subject.py
+++ app/models/subject.py
@@ -1,6 +1,7 @@
 from sqlalchemy import (
     Column,
     String,
+    Boolean
 )
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import SmallInteger
@@ -10,5 +11,8 @@ from app.db.base_class import Base
 class Subject(Base):
     id = Column(SmallInteger, primary_key=True, index=True, nullable=False)
     name = Column(String, unique=True, index=True, nullable=False)
-
-    grades = relationship('GradeSubject', back_populates='subject', cascade="all, delete-orphan")
+    add_to_total = Column(Boolean, default=True, nullable=False)
+    higher_score = Column(SmallInteger, default=100)
+    lower_score = Column(SmallInteger, default=50)
+    grades = relationship(
+        'GradeSubject', back_populates='subject', cascade="all, delete-orphan")
