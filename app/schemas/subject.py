c2a0b4d90a82943a9aee17cec166fbb6392634b6
“ho011” <zzzsssx0@gmail.com>
2022-12-27 04:14:01 +0000

    Edit: change subject schema

--- app/schemas/subject.py
+++ app/schemas/subject.py
@@ -1,7 +1,12 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class SubjectBase(BaseModel):
     name: str
+    add_to_total: bool = True,
+    higher_score: int = 100
+    lower_score: int = 50
+
 
 class SubjectCreate(SubjectBase):
     pass
